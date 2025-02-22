24aeef95082cb7b9eaa814420a038655033f1a73
Arshad-Shamim <arshadshamim555@gmail.com>
2025-02-22 20:44:44 +0530

    80% complete

--- client/src/pages/services/MobileDev.jsx
+++ client/src/pages/services/MobileDev.jsx
@@ -47,6 +47,60 @@ export default function MobileDev() {
                 </div>
             </div>
         </section>
+        
+        
+        <section className='container-fluid row' style={{background:"linear-gradient(90deg, rgb(66, 225, 214), rgb(29, 48, 130))",minHeight:"350px"}}>
+            <div className='col-md-6 col-12 d-flex py-4' style={{minHeight:"350px"}}>
+                <div className='col-11 border mx-auto my-auto  py-4 ps-3 rounded text-white border-2'>
+                    <div className='col-10 py-4'>
+                        <h4 className='border-bottom fw-bold'>01.Professional Web Development</h4>
+                        <h4 className='border-bottom fw-bold'>02.Mobile-First Web Development</h4>
+                        <h4 className='border-bottom fw-bold'>03.Performance Optimization</h4>
+                        <h4 className='border-bottom fw-bold'>04.Web Apps & WPA</h4>
+                        <h4 className='border-bottom fw-bold'>05.Responsive Web Design</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className='col-md-6 col-12 d-flex'>
+                <div className='col-10 col-md-11  my-md-4 pt-md-3 text-white mx-md-0 mx-auto'>
+                    <h1>Mobile App Development</h1>
+                    <p>At Ameotech Informatics, we craft custom iOS and apps that offer seamless, intuitive experiences. From concept to launch, we deliver scalable, high-performance solutions tailored to your business goals. </p>
+                </div>
+            </div>
+        </section>
+
+
+        <section>
+            <div class="container customize-box py-5 my-4">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-between flex-wrap">
+                        <div class="col-md-3">
+                            <div class="number-count text-center">
+                                <h4>17+</h4>
+                                <p>Years in Custom Software Development.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="number-count text-center">
+                                <h4>2000+</h4>
+                                <p>Successfully Delivered Software Projects.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="number-count text-center">
+                                <h4>95%</h4>
+                                <p>Customer Retention Rate Metrics..</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-center heading-text mt-5">
+                        <a href="/contactus" class="btn rounded-pill">Discuss Your Design Requirements</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </>
   )
 }
